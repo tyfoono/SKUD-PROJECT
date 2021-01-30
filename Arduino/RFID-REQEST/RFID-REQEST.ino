f5ba5ac1f1a63735574c1cfb12ae4ff47840ca20
tyfoono <78162478+tyfoono@users.noreply.github.com>
2021-01-30 19:53:07 +0300

    Update RFID-REQEST.ino

--- Arduino/RFID-REQEST/RFID-REQEST.ino
+++ Arduino/RFID-REQEST/RFID-REQEST.ino
@@ -24,6 +24,37 @@ int    HTTP_PORT   = 4567;
 String HTTP_METHOD = "GET"; // or POST
 char   HOST_NAME[] = "192.168.1.153";
 String ROUTE   = "/read";
+  char answer() {
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.print(c);
+      }
+    }
+  }
+
+void sendargs(String argname, String arg) {
+  
+    String ID = stringID(uid);
+    Serial.println();
+    if (client.connect(HOST_NAME, HTTP_PORT)) {
+      Serial.println("Connected to server");
+
+      client.println(HTTP_METHOD + " " + ROUTE + argname + "?=" + arg + " HTTP/1.1");
+      client.println("Host: " + String(HOST_NAME));
+      client.println("Connection: close");
+      client.println();
+      answer();
+
+      client.stop();
+      Serial.println();
+      Serial.println("disconnected");
+    } else {// if not connected:
+      Serial.println("connection failed");
+    }
+
+
+}
 
 void setup() {
   Serial.begin(9600);
@@ -54,43 +85,14 @@ void loop() {
   uint8_t uid[4];
   uint8_t uidLength;
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-
-  String stringID(uint8_t[] uid) {
+  sendargs("card", ID);
+  if(success){
+    
+  }
+}
+String stringID(uint8_t[] uid) {
     String CardID = "";
     for (byte i = 0; i < 4; i++)
       CardID += String(uid[i], HEX);
     return CardID;
   }
-  char answer() {
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.print(c);
-      }
-    }
-  }
-}
-
-void sendargs(String argname, String arg) {
-  if (success) {
-    String ID = stringID(uid);
-    Serial.println();
-    if (client.connect(HOST_NAME, HTTP_PORT)) {
-      Serial.println("Connected to server");
-
-      client.println(HTTP_METHOD + " " + ROUTE + argname + "?=" + arg + " HTTP/1.1");
-      client.println("Host: " + String(HOST_NAME));
-      client.println("Connection: close");
-      client.println();
-      answer();
-
-      client.stop();
-      Serial.println();
-      Serial.println("disconnected");
-    } else {// if not connected:
-      Serial.println("connection failed");
-    }
-
-  }
-
-}
