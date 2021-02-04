87546fbe651021dbf514540051c4eeb4ccba1b7f
tyfoono <78162478+tyfoono@users.noreply.github.com>
2021-02-04 19:04:46 +0300

    1

--- Python/server.py
+++ Python/server.py
@@ -1,25 +1,25 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
 server = Flask(__name__)
-
-cards = []
-
+db = open("cards.txt", "r")
+print(db.readline(1))
 @server.route('/')
 def hello():
     return "Hello World!"
 
+@server.route('/read')
+def read():
+    
+    card = request.args.get('card')
+    print("ID карты:", card)
+    #Эта проверка работает. Проблема была в типе данных
+    if card in cards:
+        return "Проходите"
+    else:
+        return "Прохода нет"
 
-@server.route('/david')
-def david():
-    return "Hello David!"
-
-@server.route('/check')
-def check():
-    answer = dict()
-    cardID = request.args.get('cardID')
-    if cardID in cards:
-        answer['status'] = 200
-        answer['message'] = "Проходите"
-    return jsonify(answer)
 
 if __name__ == '__main__':
+    #192.168.1.153
     server.run(host='192.168.1.153', port=4567)
+
+db.close()
