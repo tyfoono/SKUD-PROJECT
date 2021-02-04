45cddd194bcfc206de254d54345170b770d165ab
tyfoono <78162478+tyfoono@users.noreply.github.com>
2021-02-04 18:10:14 +0300

    1

--- Python/test.py
+++ Python/test.py
@@ -11,13 +11,17 @@ def hello():
 def read():
     cards = [10, 1, 3, 2]
     card = request.args.get('card')
-    print(card)
+    print("ID карты:", card)
     #эта проверка не работает
     if card in cards:
         return "Проходите"
     else:
         return "Прохода нет"
-    ''''''
+    '''
+    Проблема именно в проверке.
+    ID карты считывается правильно.
+    Либо не код не видит список, либо я проблема в коде 
+    '''
 
 if __name__ == '__main__':
     #192.168.1.153
