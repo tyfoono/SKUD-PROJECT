dc079235ec9f436a599d672a8fb6689a8eaab576
tyfoono <78162478+tyfoono@users.noreply.github.com>
2021-02-04 19:26:44 +0300

    Update server.py

--- Python/server.py
+++ Python/server.py
@@ -1,17 +1,16 @@
 from flask import Flask, request
 server = Flask(__name__)
-db = open("data/cards.txt", "r")
-print(db.readline(1))
+
+
 @server.route('/')
 def hello():
     return "Hello World!"
 
 @server.route('/read')
 def read():
-    
+    cards = ["138e25d8", "337c28d8"]
     card = request.args.get('card')
     print("ID карты:", card)
-    #Эта проверка работает. Проблема была в типе данных
     if card in cards:
         return "Проходите"
     else:
@@ -21,5 +20,3 @@ def read():
 if __name__ == '__main__':
     #192.168.1.153
     server.run(host='192.168.1.153', port=4567)
-
-db.close()
